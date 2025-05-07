8379111e70a58c62123fc9f3f07bf712d2ceff04
Alyah Davis-Moeck <alyah.davismoeck@stud.fhgr.ch>
2025-05-07 21:11:18 +0200

    API Fetch getestet, Code drinnen

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 console.log("hoi script.js");
 // Was wir brauchen aus den Daten:
-//Link zu Quotes Southpark, Buddha und Affirmations API
+//Link zu Quotes South Park, Game of Thrones und Advice Slip API
+
+//Fetch API
+const url = "https://api.adviceslip.com/advice";
+try {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data); // gibt die Daten der API in der Konsole aus
+} catch (error) {
+  console.error(error);
+}
